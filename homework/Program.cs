8efa873cdbefa6c6410cd10dd626d267a88dc322
Liydmila <mila.ml@mail.ru>
2023-04-19 08:01:09 +0300

    SortArray

---
+++ homework/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+
+
+
+string [] CreateArray(int rows)
+{   
+    string [] newArray = new string [rows];
+    
+    for(int i=0; i<rows; i++)
+    {
+            Console.WriteLine("Input "+i+" element array ");
+            newArray[i] = Console.ReadLine();
+
+            
+    }
+
+    return newArray;
+}
+
+void SortArray (string [] longArray)
+{
+    for(int i=1; i<=longArray.GetLength(0); i++)
+    {
+        if(longArray[i].Length<=3) Console.WriteLine(longArray[i]+" ,");
+    }
+}
+
+Console.WriteLine("Input size array ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+CreateArray(num);
